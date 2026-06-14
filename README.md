6cec21a1122f0dbb6ddd7b94baebf2fdb92146bc
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-06-14 17:25:13 -0400

    add digital signature basic code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 As a way of understanding a topic someone should be able to do experiments around it, write about it, etc. 
 
-One of the goals is to represent the information in a way that allows algorithms like PageRank or a retrieval information system to be used.
+One of the goals is to represent the information in a way that allows algorithms like PageRank or a retrieval information system to use it.
---
+++ self/from_ops_to_digital_signatures/digital_signature/digital_signature.py
@@ -0,0 +1,22 @@
+from self.from_ops_to_digital_signatures.rsa import rsa_api
+
+
+class DigitalSignature:
+    def __init__(self, bits=1024):
+        self.bits = 1024
+        self.private_key, self.public_key = rsa_api.generate_keys(self.bits)
+
+    def sign(self, message: str) -> str:
+        hashed_message = DigitalSignature.hash_message(message)
+        signed_message = rsa_api.encrypt(hashed_message, self.private_key)
+        return signed_message
+
+    @staticmethod
+    def hash_message(message: str) -> str:
+        return ""
+
+    @staticmethod
+    def check_signed_data(message: str, signed_message: str, public_key: str) -> bool:
+        hashed_message = DigitalSignature.hash_message(message)
+        decrypted_message = rsa_api.decrypt(signed_message, public_key)
+        return hashed_message == decrypted_message
